1e6a28b8e50b20a163d28f437eed4fa09565bc37
cjh <cjh@cjhdeMacBook-Pro-2.local>
2025-12-26 16:24:36 +0800

    recover bugs

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,21 +4,21 @@ export const projects = [
     title: "!Let's draw!",
     description: "",
     tech: ["cpp", "cmake", "makefile"],
-    image: "/assets/draw.jpg",
+    image: "../assets/draw.jpg",
     github: "https://github.com/zoya-Heyan/Draw-your-own-painting",
   },
   {
     title: "static page for transforming",
     description: "png --> jpg or jpg --> png",
     tech: ["html", "CSS", "javascript"],
-    image: "/assets/trans.jpg",
+    image: "../assets/trans.jpg",
     github: "https://github.com/zoya-Heyan/Static-websites-realise-format-conversion",
   },
   {
     title: "mini search engine linked to a database",
     description: "",
     tech: ["java", "spring boot", "mySQL"],
-    image: "/assets/searchEngine.png",
+    image: "../assets/searchEngine.png",
     github: "https://github.com/zoya-Heyan/websearch"
   },
 ];
